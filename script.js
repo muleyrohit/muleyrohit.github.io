2856bd6747365ecbd68fdaefd232be93879a0d7a
muleyrohit <31532431+muleyrohit@users.noreply.github.com>
2021-06-05 18:47:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+ // the used links
+var links = [
+    "bbc.com",
+    "google.com",
+    "youtube.com",
+    "facebook.com",
+    "",
+    ""
+
+];
+
+openStuff = function () {
+    // get a random number between 0 and the number of links
+    var randIdx = Math.random() * links.length;
+    // round it, so it can be used as array index
+    randIdx = parseInt(randIdx, 10);
+    // construct the link to be opened
+    var link = 'http://' + links[randIdx];
+    // open it in a new window / tab (depends on browser setting)
+    window.open(link);
+};
