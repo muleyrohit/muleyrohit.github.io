512cb7f15bb5ba77cde5dde58462df66495ba121
muleyrohit <31532431+muleyrohit@users.noreply.github.com>
2021-06-05 20:40:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
  // the used links
 var links = [
-    "bbc.com",
-    "google.com",
-    "youtube.com",
-    "facebook.com",
-    "muleyrohit.github.io/Quotes/",
-    "google.com"
+    "muleyrohit.github.io/Quotes/1.html",
+    "muleyrohit.github.io/Quotes/2.html",
+    "muleyrohit.github.io/Quotes/3.html",
+    "muleyrohit.github.io/Quotes/4.html",
+    "muleyrohit.github.io/Quotes/5.html",
+    "muleyrohit.github.io/Quotes/6.html",
+    "muleyrohit.github.io/Quotes/7.html",
+    "muleyrohit.github.io/Quotes/8.html"
 
 ];
 
@@ -15,7 +17,7 @@ openStuff = function () {
     // round it, so it can be used as array index
     randIdx = parseInt(randIdx, 10);
     // construct the link to be opened
-    var link = 'http://' + links[randIdx];
+    var link = 'https://' + links[randIdx];
     // open it in a new window / tab (depends on browser setting)
     window.open(link, "_self");
 };
